6a13e528ed84e1536f28536bf77de39bac533c36
James Reggio <james.reggio@gmail.com>
2016-03-11 16:48:21 -0500

    Modify to cope with Chrome 38's always-visible page actions

--- src/background.js
+++ src/background.js
@@ -12,42 +12,17 @@ const util = require('./util');
  */
 chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
   const parsed = util.parseUrl(tab.url);
-  if (parsed.validHost) {
-    if (parsed.validPath) {
-      chrome.pageAction.setIcon({
-        tabId,
-        path: {
-          19: 'img/icon-19.png',
-          38: 'img/icon-38.png',
-        },
-      });
-      chrome.pageAction.setTitle({
-        tabId,
-        title: 'Click to extract crash',
-      });
-      chrome.pageAction.setPopup({
-        tabId,
-        popup: 'popup.html',
-      });
-    } else {
-      chrome.pageAction.setIcon({
-        tabId,
-        path: {
-          19: 'img/icon-disabled-19.png',
-          38: 'img/icon-disabled-38.png',
-        },
-      });
-      chrome.pageAction.setTitle({
-        tabId,
-        title: 'Browse to an individual crash to use this extension',
-      });
-      chrome.pageAction.setPopup({
-        tabId,
-        popup: '',
-      });
-    }
+  if (parsed.validPath) {
+    chrome.pageAction.setTitle({
+      tabId,
+      title: 'Click to extract crash',
+    });
     chrome.pageAction.show(tabId);
   } else {
+    chrome.pageAction.setTitle({
+      tabId,
+      title: 'Browse to an individual crash to use this extension',
+    });
     chrome.pageAction.hide(tabId);
   }
 });
